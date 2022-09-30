fde0e54fb6abaa2ad8e52ea390d1a56c847530cc
felipefporto <felipe.porto@fatec.sp.gov.br>
2022-09-30 12:17:26 -0300

    Enviando nova organização de Pastas e conclusão do primero arquivo .py

---
+++ Bootcamp Ciência de Dados Unimed-BH/Python/primeiro_programa.py
@@ -0,0 +1,4 @@
+#Primeiro Programa em Python do Bootcamp Unimed-BH
+print("Hello World!")
+print("Obrigado pela dedicação da dio.me em proporcionar novas oportunidades para todos.")
+print("Muito Obrigado!")
